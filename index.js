44d1f02742ee900ce793fc5169e5e930bf5df8c4
Jevin <therealjevin@gmail.com>
2023-01-11 08:47:26 -0600

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // code your solution here
+function superbowlWin(records) {
+    const firstWinningRecord = records.find(record => record.result === "W");
+    if(typeof firstWinningRecord === "undefined") {
+        return firstWinningRecord;
+    }
+    return firstWinningRecord.year;
+}
